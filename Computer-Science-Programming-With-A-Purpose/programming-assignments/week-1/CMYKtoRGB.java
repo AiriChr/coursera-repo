cbe546c3a17399b3906d28adc08b9c6837d9ddbc
AiriChr <44491497+AiriChr@users.noreply.github.com>
2023-09-03 19:21:57 +0800

    Update CMYKtoRGB.java

--- Computer-Science-Programming-With-A-Purpose/programming-assignments/week-1/CMYKtoRGB.java
+++ Computer-Science-Programming-With-A-Purpose/programming-assignments/week-1/CMYKtoRGB.java
@@ -6,18 +6,22 @@
 
 public class CMYKtoRGB {
     public static void main(String[] args) {
+        // input values for cyan, magenta, yellow and black respectively
         double cyan = Double.parseDouble(args[0]);
         double magenta = Double.parseDouble(args[1]);
         double yellow = Double.parseDouble(args[2]);
         double black = Double.parseDouble(args[3]);
 
-        double white = (1 - black);
-        double red = 255 * white * (1 - cyan);
-        double green = 255 * white * (1 - magenta);
-        double blue = 255 * white * (1 - yellow);
+        // perform mathematical operation and typecast to int
+        int white = (int) (1 - black);
+        int red = (int) (255 * white * (1 - cyan));
+        int green = (int) (255 * white * (1 - magenta));
+        int blue = (int) (255 * white * (1 - yellow));
 
-        System.out.println("red = " + Math.round(red));
-        System.out.println("green = " + Math.round(green));
-        System.out.println("blue = " + Math.round(blue));
+        // output RGB values
+        System.out.println("red = " + red);
+        System.out.println("green = " + green);
+        System.out.println("blue = " + blue);
     }
 }
+
